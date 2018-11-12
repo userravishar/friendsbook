7f3bce5b15b2895d1cf87a34a61f7ffc459d9a00
gbadoni <gauravbadoni@gmail.com>
2018-11-11 20:53:26 -0800

    test program to read etcd

---
+++ playground/src/com/etcd/TestReadDB.java
@@ -0,0 +1,15 @@
+package com.etcd;
+
+public class TestReadDB {
+
+	public TestReadDB() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Etcd db = new Etcd();
+		db.queryCPUStats();
+	}
+
+}
